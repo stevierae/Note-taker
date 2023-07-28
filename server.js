6d00c53957cb1de1f62e651b91430b95343e7181
Stevie <stefaniefisher88@gmail.com>
2023-07-28 16:29:48 -0400

    added middleware

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// server middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
