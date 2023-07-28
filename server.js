8e7183a3cf8daa337b55a34507dd3f4b4124649a
Stevie <stefaniefisher88@gmail.com>
2023-07-28 16:35:24 -0400

    completed html routes

--- server.js
+++ server.js
@@ -12,6 +12,21 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+// html routes
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
